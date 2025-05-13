ebccb2c85177bbaf71181dd93af5b46ea0498210
SophiaTaddeoN <sophiataddeon@gmail.com>
2025-05-13 16:36:39 -0300

    gpt

--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -22,17 +22,51 @@ app.get('/',(req,res) => {
 
 //ATV GET POST TEMPLATE
 
-app.get('/login',(req,res) => {
-    res.redirect("/Projetos_Passados/Get_Post_Template/login.html")
+app.get('/usuario',(requisicao,resposta) =>{ 
+    resposta.redirect("/Projetos_Passados/Get_Post_Template/usuario.html")
 })
 
-app.get('/cadastra',(req,res) => {
-    res.redirect("/Projetos_Passados/Get_Post_Template/cadastro.html")
+
+app.post('/resposta_cadastro', (requisicao,resposta) =>{
+    resposta.redirect("/Projetos_Passados/Get_Post_Template/login.html")
 })
 
-app.post('/resposta_ejs',(req,res) => {
-    let login = req.body.login;
-    let senha = req.body.senha;
+
+app.get("/cadastrar", function(requisicao, resposta){
+    let nome = requisicao.query.nome;
+    let login = requisicao.query.login;
+    let senha = requisicao.query.senha;
+    let nasc = requisicao.query.nascimento;
+
+    console.log(nome, login, senha, nasc)
+    resposta.redirect("/Projetos_Passados/Get_Post_Template/cadastro.html")
+})
+
+
+app.post('/logar', function(requisicao, resposta){
+    let login = requisicao.body.login;
+    let senha = requisicao.body.senha;
     console.log(login, senha);
-    res.redirect("/views/resposta.ejs")
+
+    var data = {db_login: login, db_senha: senha}
+
+    usuarios.find(data).toArray(function(err, items){
+        console.log(items)
+        if(items.length == 0){
+            resposta.render("resposta_login",{status: "usuario/senha não encontrado"});
+        }else if(err){
+            resposta.render("resposta_login",{status: "erro ao logar"});
+        }else{
+            resposta.render("resposta_login",{status: "usuario "+login+" logado"});
+        }
+    })
+
+})
+
+app.post('/resposta_ejs', (requisicao,resposta) =>{
+    resposta.redirect("/views/resposta.ejs")
+})
+
+app.get('/resposta_ejs', (requisicao,resposta) =>{
+    resposta.redirect("/views/resposta.ejs")
 })
